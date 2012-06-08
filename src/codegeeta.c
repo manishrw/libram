913392b77b7389325279598023d999551d296a69
PioneerAxon <arth.svnit@gmail.com>
2012-06-09 02:24:24 +0530

    Error reporting.
    ADDED : Error reporting functions.
    FIXED : README. This is a static library.

--- src/codegeeta.c
+++ src/codegeeta.c
@@ -1,4 +1,23 @@
+#include<stdio.h>
 #include<codegeeta.h>
 
-
 //All function definitions here.. :D
+
+//error callback function pointer.
+void (*cg_error_callback) (unsigned int, char*);
+
+//Register callback function.
+void cg_register_error_callback (void (*function) (unsigned int, char*))
+{
+	cg_error_callback = function;
+}
+
+//Error reporting.
+void cg_error (unsigned int errno, char* message)
+{
+	// Check if the error reporting function is registered.
+	if (cg_error_callback)
+		(*cg_error_callback) (errno, message);
+	else
+		fprintf (stderr, "ERROR %x : %s\n", errno, message);
+}
--- src/codegeeta.h
+++ src/codegeeta.h
@@ -1,6 +1,18 @@
 #ifndef __CODE_GEETA
 #define __CODE_GEETA
 
+//All errors go here.
+#define CG_ERROR_ARGUMENT_IS_NULL 1
+#define CG_ERROR_ARGUMENT_IS_ZERO 2
+#define CG_ERROR_ARGUMENT_IS_NEGATIVE 3
+#define CG_ERROR_ARGUMENT_OUT_OF_BOUND 4
+
 //All function declaration here.
+
+//Register an on_error callback function.
+void cg_register_error_callback (void (*function) (unsigned int, char*));
+
+//To allow user to throw errors.
+void cg_error (unsigned int, char*);
 
 #endif
