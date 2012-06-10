6c78cc3e5a5c181fb9d37ae55faa2231aa337957
manish wadhwani <manishwr@gmail.com>
2012-06-11 03:50:13 +0530

    GCD functions
    ADDED: gcd function cg_gcd_2l(), cg_gcd_nl().
    
    Signed-off-by: PioneerAxon <arth.svnit@gmail.com>

--- src/test.c
+++ src/test.c
@@ -2,8 +2,14 @@
 #include<stdlib.h>
 #include<codegeeta.h>
 
-int main()
+int main (int argc, char **argv)
 {
 	//All calls to test libcodegeeta.a here.
+	long long *arr;
+	int i;
+	arr = (long long*) malloc ((argc - 1) * sizeof(long long));
+	for (i = 1; i < argc; i++)
+		arr [i - 1] = atoll (argv [i]);
+	printf(" Ans: %Ld \n", cg_gcd_nl (arr, argc - 1));
 	return 0;
 }
