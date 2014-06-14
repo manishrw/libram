ec09167646bd6887253c00a62953bf7bc59b0bfb
manishrw <manishwr@gmail.com>
2014-06-14 18:08:00 +0530

    ADDED template STL header file

---
+++ src/template.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+
+#define MOD 1000000007
+#define PIE 3.14159265358979323846264338327950
+#define LL long long int
+#define VI vector<int>
+#define VLL vector<long long int>
+#define MII map<int, int>
+#define PII pair<int, int>
+#define PCI pair<char, int>
+#define FT first
+#define SD second
+#define BG begin()
+#define ED end()
+#define PB push_back
+#define INSM(x,y) insert(PII(x, y)
+#define INSMC(x,y) insert(PCI(x, y)
+#define ALL(v) v.BG, v.ED
+#define RSZ(v,n) v.resize(n)
+#define ERS(v) v.erase(ALL(v))
+#define MS(v) memset(v, 0, sizeof(v))
+#define FOR(i,a,b) for(i=a;i<b;i++)
+#define FR(i,a,b) for(i=a;i<=b;i++)
+#define REP(x,y) FOR(x,0,y)
+// input and output
+#define scan(x) scanf("%d",&x)
+#define scanll(x) scanf("%lld",&x)
+#define print(x) printf("%d\n",x)
+#define printll(x) printf("%lld\n",x)
+#define f_in(st) freopen(st,"r",stdin)
+#define f_out(st) freopen(st,"w",stdout)
